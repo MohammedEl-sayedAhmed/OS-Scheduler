47b9294d52139379e8fd87b1839f4aad18105149
MohammedEl-sayedAhmed <mohammedelsayed4648@gmail.com>
2020-03-24 01:20:09 +0200

    change pcb.h to ProcessFromInput and add some comments in process_generator.c

--- process_generator.c
+++ process_generator.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 #include "headers.h"
-#include "PCB.h"
+#include "ProcessFromInput.h"
 
 
 void clearResources(int);
@@ -34,33 +34,33 @@ void clearResources(int signum)
 //1. Read the input files.
 void readInputFile()
 {
-    FILE *inputFile;
-    inputFile = fopen("processes.txt", "r");
-    char processData[20];
+    FILE *inputFile; 
+    inputFile = fopen("processes.txt", "r"); // open the input file in a read mode
+    char intputFileLine[20]; // assign the lines in the txt file to processData
 
    while (!feof(inputFile) ) {
 
-        processData[0] = fgetc(inputFile); // fgetc() is used to obtain input from a file single character at a time
+        intputFileLine[0] = fgetc(inputFile); // fgetc() is used to obtain input from a file single character at a time
         
-        if (processData[0] != '#'){ // if it is not #, then it is a process
+        if (intputFileLine[0] != '#'){ // if it is not #, then it is a process
 
             // this is a process -- save it in whatever stuff (will be a queue later)
-            PCB * myNewPCB = (PCB *) malloc(sizeof(PCB)); // malloc >> memory allocation, it is like new in c++
+            ProcessFromInput * myNewProcess = (ProcessFromInput *) malloc(sizeof(ProcessFromInput)); // malloc >> memory allocation, it is like new in c++
           
             // save the ID first, because there is a bug that makes the ID= 0 many times so we want to check for it
             // Also use atoi; to convert the string to int -- like %d in fscanf 
-            myNewPCB->id = atoi(processData); 
+            myNewProcess->id = atoi(intputFileLine); 
 
-            if (myNewPCB->id == 0){
+            if (myNewProcess->id == 0){
                 continue; // to skip the 0 process id that comes in between
             }
 
-            fscanf(inputFile , "%d %d %d", &myNewPCB->arrivalTime, &myNewPCB->runTime , &myNewPCB->priority); // read the process data and save it proberly to the pcb struct object
+            fscanf(inputFile , "%d %d %d", &myNewProcess->arrivalTime, &myNewProcess->runTime , &myNewProcess->priority); // read the process data and save it proberly to the pcb struct object
 
-            printf("The ID is %d\n", myNewPCB->id);
-            printf("The arrival time is %d\n", myNewPCB->arrivalTime);
-            printf("The run time is %d \n", myNewPCB->runTime);
-            printf("The priority is %d \n\n\n", myNewPCB->priority);
+            printf("The ID is %d\n", myNewProcess->id);
+            printf("The arrival time is %d\n", myNewProcess->arrivalTime);
+            printf("The run time is %d \n", myNewProcess->runTime);
+            printf("The priority is %d \n\n\n", myNewProcess->priority);
 
 
         }
