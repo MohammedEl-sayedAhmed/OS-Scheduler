e373be705e333214ec4c1775179f1173e8b058ab
MohammedEl-sayedAhmed <mohammedelsayed4648@gmail.com>
2020-03-24 05:25:45 +0200

    Add struct PCB.h

---
+++ PCB.h
@@ -0,0 +1,20 @@
+#pragma once 
+
+#include <ProcessFromInput.h>
+#include <stdlib.h>
+
+typedef struct 
+{
+    ProcessFromInput newArrivedProcess;
+    pid_t pid;
+    int startTime;
+    int remainingTime;
+    int finishTime;
+    enum {STARTED, RESUMED, STOPED, FINISHED};
+
+}   PCB;
+
+
+
+// I think that i will add here initilization for this struct also 
+// may be like a function || will figure it out later
